2f93baa7125273eefb94e842ea34808017bfe1d0
Philip <bergstrom.philip@gmail.com>
2016-07-07 07:22:20 +0200

    Changes to fileactivity

--- app/src/main/java/com/bjtu/al/summerschool/FileActivity.java
+++ app/src/main/java/com/bjtu/al/summerschool/FileActivity.java
@@ -1,14 +1,11 @@
 package com.bjtu.al.summerschool;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.NotificationCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -28,17 +25,18 @@ import android.widget.TextView;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 
 public class FileActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-
     private static final String LOG_TAG = "AudioRecordTest";
     private MediaPlayer mPlayer = null;
     private Handler mHandler = new Handler();
     private static int save = -1;
+    private String currentFile;
 
     private void startPlaying(String mFileName) {
         mPlayer = new MediaPlayer();
@@ -46,8 +44,6 @@ public class FileActivity extends AppCompatActivity
             mPlayer.setDataSource(FileManager.path + "/" + mFileName);
             mPlayer.prepare();
             mPlayer.start();
-            createPlayNotification();
-
         } catch (IOException e) {
             Log.e(LOG_TAG, "prepare() failed");
         }
@@ -84,25 +80,46 @@ public class FileActivity extends AppCompatActivity
         final ListView listView = (ListView) findViewById(R.id.listView);
         final TextView progressView = (TextView) findViewById(R.id.progressView);
         final ImageButton playbutton = (ImageButton) findViewById(R.id.playButton2);
-
-        ArrayAdapter itemsAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, FileManager.GetFiles());
+        final ImageButton nextButton = (ImageButton) findViewById(R.id.nextButton);
+        final ImageButton prevButton = (ImageButton) findViewById(R.id.prevButton);
+        final ImageButton deletebutton = (ImageButton) findViewById(R.id.deleteButton);
+        ArrayList<String> lst = new ArrayList<String>(Arrays.asList(FileManager.GetFiles()));
+        final ArrayAdapter<String> itemsAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, lst);
         listView.setAdapter(itemsAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
             public void onItemClick(AdapterView<?> parent, View v,int position, long arg3)
             {
+                //listView.setSelector(new ColorDrawable(Color.parseColor("#51B7A6")));
+                //listView.setItemChecked(position, true);
+                //listView.setSelection(position);
+                Log.i("Test FileActivity", Integer.toString(position));
                 stopPlaying();
+                deletebutton.setVisibility(View.VISIBLE);
                 save = position;
-                String str = ((TextView) v).getText().toString();
-                startPlaying(str);
+                currentFile = ((TextView) v).getText().toString();
+                startPlaying(currentFile);
+                mSeekBar.setMax(mPlayer.getDuration());
                 mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                     public void onCompletion(MediaPlayer mp) {
                         playbutton.setBackgroundResource(R.drawable.ic_play_circle_filled_black_48dp);
                     }
                 });
-                playbutton.setBackgroundColor(Color.TRANSPARENT);
                 playbutton.setBackgroundResource(R.drawable.ic_pause_circle_filled_black_48dp);
-                mSeekBar.setMax(mPlayer.getDuration());
+
+                if (save >= listView.getAdapter().getCount())
+                {
+                     nextButton.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    nextButton.setVisibility(View.VISIBLE);
+                }
+                if (save < 1) {
+                    prevButton.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    prevButton.setVisibility(View.VISIBLE);
+                }
             }
         });
         playbutton.setOnClickListener(new View.OnClickListener() {
@@ -120,17 +137,57 @@ public class FileActivity extends AppCompatActivity
                 }
             }
         });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                save += 1;
+                listView.performItemClick(
+                        listView.getAdapter().getView(save, null, null),
+                        save,
+                        listView.getAdapter().getItemId(save));
+            }
+        });
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                save -= 1;
+                listView.performItemClick(
+                        listView.getAdapter().getView(save, null, null),
+                        save,
+                        listView.getAdapter().getItemId(save));
+            }
+        });
+
+        deletebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                if (currentFile == null) {
+                    return;
+                }
+                File file = new File(FileManager.path + "/" + currentFile);
+                file.delete();
+                itemsAdapter.remove(currentFile);
+                playbutton.setBackgroundColor(Color.TRANSPARENT);
+                deletebutton.setBackgroundColor(Color.TRANSPARENT);
+                nextButton.setBackgroundColor(Color.TRANSPARENT);
+                prevButton.setBackgroundColor(Color.TRANSPARENT);
+
+                //listView.setSelector(new ColorDrawable(Color.TRANSPARENT));
+                itemsAdapter.notifyDataSetChanged();
+                mPlayer.reset();
+                currentFile = null;
+            }
+        });
 
         FileActivity.this.runOnUiThread(new Runnable() {
 
             @Override
             public void run() {
                 if(mPlayer != null){
-                    int mCurrentPosition = mPlayer.getCurrentPosition() / 1000;
                     mSeekBar.setProgress(mPlayer.getCurrentPosition());
-                    progressView.setText(ToMinutes((mCurrentPosition*1000)));
+                    progressView.setText(ToMinutes(mPlayer.getCurrentPosition()));
                 }
-                mHandler.postDelayed(this, 10);
+                mHandler.postDelayed(this, 100);
             }
         });
         mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -146,16 +203,22 @@ public class FileActivity extends AppCompatActivity
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 if(mPlayer != null && fromUser){
-                    mPlayer.seekTo(progress * 1000);
+                    mPlayer.seekTo(progress);
                 }
             }
         });
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        /*
+        listView.performItemClick(
+                listView.getAdapter().getView(save, null, null),
+                save,
+                listView.getAdapter().getItemId(save));
+        mPlayer.pause();*/
 
     }
+
     public String ToMinutes(int millis) {
         return (new SimpleDateFormat("mm:ss")).format(new Date(millis));
-
-
     }
 
     @Override
@@ -164,48 +227,13 @@ public class FileActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_slideshow) {
-
         } else if (id == R.id.nav_manage) {
-            Intent intent = new Intent(FileActivity.this, SettingsActivity.class);
-            startActivity(intent);
         } else if (id == R.id.nav_record) {
             Intent intent = new Intent(FileActivity.this, MainActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_translate) {
-            Intent intent = new Intent(FileActivity.this, TranslateActivity.class);
             startActivity(intent);
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
-    public void createPlayNotification() {
-        final NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.mipmap.play)
-                        .setContentTitle("Recorder App Notification")
-                        .setContentText("App is playing");
-
-
-        Intent resultIntent = new Intent(this, MainActivity.class);
-        // Because clicking the notification opens a new ("special") activity, there's
-        // no need to create an artificial back stack.
-        PendingIntent resultPendingIntent =
-                PendingIntent.getActivity(
-                        this,
-                        0,
-                        resultIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        mBuilder.setContentIntent(resultPendingIntent);
-        // Sets an ID for the notification
-        int mNotificationId = 002;
-        // Gets an instance of the NotificationManager service
-        NotificationManager mNotifyMgr =
-                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        // Builds the notification and issues it.
-        mNotifyMgr.notify(mNotificationId, mBuilder.build());
-    }
-
 }
